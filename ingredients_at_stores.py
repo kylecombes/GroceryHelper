7c155fd85f78bc4942f79203ede1d9ded74d92b2
Nina Tchirkova <ntchirkova@gmail.com>
2017-04-10 19:31:36 -0400

    finished methods

--- ingredients_at_stores.py
+++ ingredients_at_stores.py
@@ -4,6 +4,8 @@ import requests
 
 
 def get_ingredients(ingredients, store_id):
+    """Given a list of ingredients and a store id, returns a list of ingredients at that store
+    * each ingredient is a dictionary with a keys: category, item_id and name""""
     #store_id is store id
     foods_at_store = []
     for food in ingredients:
@@ -11,19 +13,28 @@ def get_ingredients(ingredients, store_id):
         xml_string = requests.get(url).text
         foods = untangle.parse(xml_string)
         for item in foods.ArrayOfProduct.Product:
-            store = {
-                'aisle': item.AisleNumber.cdata,
+            f = {
                 'category': item.ItemCategory.cdata,
                 'item_id': item.ItemID.cdata,
-                'description': item.ItemDescription.cdata,
-                'image': item.ItemImage.cdata,
                 'name': item.Itemname.cdata
             }
-            foods_at_store.append(store)
+            foods_at_store.append(f)
     return foods_at_store
+
+def where_are_ingreidients(ingredients, stores):
+    """ Given a list of stores ids and ingredients returns dictionary of ingredients with stores_ids as values"""
+    inventory = {}
+    for s in stores:
+        foods = get_ingredients(ingredients, s)
+        for f in foods:
+            inventory[f['name']] = s
+    return inventory
+
+
 
 
 def format_food_url(store_id, food):
+    """formats url to make an api request so that """
     base_url = "http://www.SupermarketAPI.com/api.asmx/SearchForItem?APIKEY="
     store = "&StoreId=" + str(store_id)
     food = "&ItemName=" + food
