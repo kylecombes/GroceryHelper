6810d208601e004ae4942cbc53434b5ef1fb58fe
elenameyerson <elena.meyerson@students.olin.edu>
2017-04-19 12:39:09 -0400

    finished writing class and returnInfo function

--- recipes.py
+++ recipes.py
@@ -4,16 +4,18 @@
 """
 Defines code that takes in recipe or cuisine from user and outputs
 a list of ingredients that can be used by store finder algorithm.
-URL coded for Yummly API, still waiting to hear back with API key though.
+URL coded for use with Yummly API.
+Defines Recipe class that holds information about each recipe (name, ID,
+ingredients, cuisine, source website, and image)
 """
 
 from urllib import urlopen
 import json
 
-def userInput():
-    pass
-    ###integrate with Nina's code but add extra input sections
 
+"""
+Encodes URL using user input for use with Yummly API.
+"""
 
 def encodeURL_Yummly(search, allowed_ingredients, allowed_cuisines):
     f = open('keys.py')
@@ -38,17 +40,11 @@ def encodeURL_Yummly(search, allowed_ingredients, allowed_cuisines):
     url = BASE_URL + "q=" + search + ingred + cuisines
     return url
 
-def encodeURL_Campbells(search, ingredients):
-    BASE_URL = "http://api.campbellskitchen.com/api/BrandService.svc/"
-    search = search.replace(" ", "%20")
 
-    inglist = ""
-    for ing in ingredients:
-        inglist = ing + "|"
-
-    url = BASE_URL + "/search?ingredient=" + inglist + "&app_id=45d2a062&app_key=%s"% (RECIPE_API_KEY)
-
-"""To Do: figure out calling api key from file, add inputs for other types of searches"""
+"""
+Calls API using formatted URL and returns data in JSON format.
+returnInformation formats data into a usable list.
+"""
 
 def callAPI(url):
     f = urlopen(url)
@@ -57,11 +53,36 @@ def callAPI(url):
     return response_data
 
 
-def returnIngredients(response_data):
-    pass
-    #ingredientLines from the output data
+def returnInformation(response_data):
+    name = response_data['matches'][0]['recipeName']
+    recipeID = response_data['matches'][0]['id']
+    ingredients = response_data['matches'][0]['ingredients']
+    cuisine = response_data['matches'][0]['attributes']['cuisine'][0]
+    source = response_data['matches'][0]['sourceDisplayName']
+    imageURL = response_data['matches'][0]['imageUrlsBySize']['90']
+    information = [name, recipeID, ingredients, cuisine, source, imageURL]
+    return information
 
 
-url = encodeURL_Yummly('fried chicken', ['apples','chicken','butter'], ['Southern'])
-print(callAPI(url))
+"""
+Defines Recipe class to hold recipe data.
+"""
+
+class Recipe():
+    def __init__(self, name, recipeID, ingredients, cuisine, source, imageURL):
+        self.name = name
+        self.recipeID = recipeID
+        self.ingredients = ingredients
+        self.cuisine = cuisine
+        self.source = source
+        self.imageURL = imageURL
+
+    def __str__(self):
+        return self.name
+
+
+#url = encodeURL_Yummly('stir fry', ['apples','chicken','butter'], ['asian'])
+#friedChicken = Recipe(returnInformation(callAPI(url))[0],returnInformation(callAPI(url))[1], returnInformation(callAPI(url))[2], returnInformation(callAPI(url))[3], returnInformation(callAPI(url))[4], returnInformation(callAPI(url))[5])
+#print(friedChicken)
+#print(callAPI(url))
 #print(encodeURL_Campbells('green', ['bean', 'chicken', 'lettuce']))
