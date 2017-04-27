d2acb48eb4c6f6afe19c8e61336b3f3321cd591a
elenameyerson <elena.meyerson@students.olin.edu>
2017-04-27 15:20:40 -0400

    find food at stores

--- ingredients_at_stores.py
+++ ingredients_at_stores.py
@@ -1,11 +1,11 @@
 from keys import *
 import untangle
 import requests
+from models import *
 
 
 def get_ingredients(ingredients, store_id):
-    """Given a list of ingredients and a store id, returns a list of ingredients at that store
-    * each ingredient is a dictionary with a keys: category, item_id and name"""
+    """Given an ingredient and a store id, returns True is item is at that store and False if it is not"""
     #store_id is store id
     foods_at_store = []
     for food in ingredients:
@@ -19,18 +19,19 @@ def get_ingredients(ingredients, store_id):
                 'name': item.Itemname.cdata
             }
             foods_at_store.append(f)
-    return foods_at_store
+    if len(foods_at_store) > 0:
+        return True
+    else:
+        return False
 
-def where_are_ingreidients(ingredients, stores):
-    """ Given a list of stores ids and ingredients returns dictionary of ingredients with stores_ids as values"""
-    inventory = {}
-    for s in stores:
-        foods = get_ingredients(ingredients, s)
-        for f in foods:
-            inventory[f['name']] = s
-    return inventory
+def where_are_ingredients(ingredients, stores):
+    """ Given a list of stores objects and ingredients returns dictionary of ingredients with stores_ids as values"""
 
-
+    for store in stores:
+        for ing in ingredients:
+            if get_ingredients(ing, store.store_id) == True:
+                store.items += [ing]
+    return stores
 
 
 def format_food_url(store_id, food):
@@ -40,6 +41,8 @@ def format_food_url(store_id, food):
     food = "&ItemName=" + food
     return base_url + SUPERMARKET_API_KEY + store + food
 
-ingredients = ["cherry"]#, "arugala", "cucumber", "coke", "cheddar cheese"]
-items_found = get_ingredients(ingredients, 'e6k3fjw75k')
-print(items_found)
+ingredients = ["cherry", "arugala", "cucumber", "coke", "cheddar cheese"]
+store = Store('e6k3fjw75k', 'Roche Bros', '1000 Olin Way')
+items_found = where_are_ingredients(ingredients, [store])
+for item in items_found:
+    print(item)
