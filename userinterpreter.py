8dd6e030d588725a3e6e47b39ef3d387ccf4a332
elenameyerson <elena.meyerson@students.olin.edu>
2017-04-28 11:11:36 -0400

    delete irrelevant files

--- userinterpreter.py
+++
@@ -1,7 +0,0 @@
-address = input("Address (street, city): ")
-state = input("State:")
-ingredients = input("List ingredients seperated by commas:")
-food_list = ingredients.rsplit(',')
-for s in food_list:
-    s = s.rstrip(' ')
-print(food_list)
